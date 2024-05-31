7323a6104acb26d314740c5fc008c6c964c756d5
IResetic <iresetic.skybit@gmail.com>
2024-06-01 00:15:21 +0200

    Add splash screen with placeholder icon

--- app/src/main/java/dev/skybit/bluetoothchat/MainActivity.kt
+++ app/src/main/java/dev/skybit/bluetoothchat/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.skybit.bluetoothchat.ui.theme.BluetoothChatTheme
 
@@ -17,6 +18,11 @@ import dev.skybit.bluetoothchat.ui.theme.BluetoothChatTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            installSplashScreen()
+        }
+
         setContent {
             BluetoothChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
